3aab4221c514c43f473edf090718265063ea5b51
Davee Sok <daveesok@gmail.com>
2021-10-02 16:00:02 -0500

    added title

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Cookie Stand Admin</title>
+      </Head>
       <h1>Home</h1>
     </>
   );
