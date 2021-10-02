72055030fa150f7d4cef70d7e47bd70af8391181
Davee Sok <daveesok@gmail.com>
2021-10-02 18:00:38 -0500

    updated forms

--- components/Form.js
+++ components/Form.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 function Form() {
   return (
-    <div>
-      <h2>Form</h2>
+    <div className='p-4 mx-8 my-8 bg-green-500 rounded-md'>
+      <h2 className='text-center'>Create Cookie Stand</h2>
+      <div>
+        <div>
+          <h2>Location</h2>
+          <input type='text' placeholder='Enter New Location Here' />
+        </div>
+        <div class='flex flex-row'>
+          <div>
+            <h2>Minimum Customer Per Hour</h2>
+            <input type='text' />
+          </div>
+          <div>
+            <h2>Maximum Customer Per Hour</h2>
+            <input type='text' />
+          </div>
+          <div>
+            <h2>Average Cookies Per Sale</h2>
+            <input type='text' />
+          </div>
+          <div>
+            <button>Create</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
