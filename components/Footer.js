64d532e803c9fe3fffb5d77793669e3f81c86999
Davee Sok <daveesok@gmail.com>
2021-10-02 16:37:31 -0500

    cleaned up header and footer slightly

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='flex items-center justify-between p-4 text-gray-100 bg-gray-500'>
+    <div className='flex items-center justify-between p-4 mt-8 text-gray-100 bg-gray-500'>
       <p className='text-xl'>8 Locations worldwide</p>
     </div>
   );
--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='flex items-center justify-between p-4 text-gray-100 bg-gray-500'>
+    <div className='flex items-center justify-between p-4 mb-8 text-gray-100 bg-gray-500'>
       <h1 className='text-2xl'>Cookie Stand Admin</h1>
       <p className='text-xl'>Overview</p>
     </div>
