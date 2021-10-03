14f938c3355e25aa6d25b2ec4ed5915ab42640a2
Davee Sok <daveesok@gmail.com>
2021-10-03 02:31:27 -0500

    added css for now

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,10 @@ function Header() {
     <nav className='sticky w-full h-auto px-8 bg-gray-200 sm:px-0'>
       <div className='container flex justify-between py-5 mx-auto'>
         <label className='text-3xl font-light text-gray-800'>
-          Cookie<span className='font-bold'>Stand</span>
+          CookieStand<span className='font-bold'>Admin</span>
         </label>
         <ul className='flex items-center text-gray-800'>
-          <li className='mr-6 font-semibold text-gray-800 uppercase transition border border-gray-800 rounded-md hover:text-gray-200 hover:bg-blue-600'>
+          <li className='mr-6 font-semibold text-gray-800 uppercase transition border border-gray-800 rounded-md hover:text-gray-200 hover:bg-gray-600'>
             <Link href='/overview'>
               <a className='block px-4 py-1'>Overview</a>
             </Link>
