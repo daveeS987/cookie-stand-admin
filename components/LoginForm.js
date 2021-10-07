afd08c297bfd3561ba7c4881dd81afef793906c0
Davee Sok <daveesok@gmail.com>
2021-10-06 19:58:10 -0500

    wired up forms

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginForm() {
+  const [formItems, setFormItems] = useState({});
+  console.log('FormItems: ', formItems);
+
+  const handleChange = (e) => {
+    const newFormItems = {
+      ...formItems,
+      [e.target.name]: e.target.value,
+    };
+    setFormItems(newFormItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('this was clicked');
+  };
+
   return (
-    <div className='w-1/2 p-8 mx-auto my-16 text-center bg-gray-200 rounded-md'>
+    <form onSubmit={handleSubmit} className='w-1/2 p-8 mx-auto my-16 text-center bg-gray-200 rounded-md'>
       <h3>Email</h3>
-      <input name='email' type='email' className='w-3/4 p-2 mt-2 rounded-md' />
+      <input name='email' type='email' className='w-3/4 p-2 mt-2 rounded-md' onChange={handleChange} />
       <h3 className='pt-8'>Password</h3>
-      <input name='password' type='password' className='w-3/4 p-2 mt-2 rounded-md' />
+      <input name='password' type='password' className='w-3/4 p-2 mt-2 rounded-md' onChange={handleChange} />
       <button className='w-3/4 p-3 mt-10 bg-gray-600 rounded-md'>Sign In</button>
-    </div>
+    </form>
   );
 }
 
