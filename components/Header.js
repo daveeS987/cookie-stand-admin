45dbf4b743348a8e536c4fbda238dafd1b249835
Davee Sok <daveesok@gmail.com>
2021-10-07 20:26:26 -0500

    wired in buttons

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useAuth } from '../contexts/auth';
 
 function Header() {
+  const { user, logout } = useAuth();
+
   return (
     <nav className="sticky w-full h-auto px-8 bg-gray-200 sm:px-0">
       <div className="container flex justify-between py-5 mx-auto">
@@ -17,11 +20,13 @@ function Header() {
               <a className="transition hover:text-blue-600">Overview</a>
             </Link>
           </li>
-          <li className="mr-6 font-semibold text-gray-800 uppercase transition border border-gray-800 rounded-md hover:text-gray-200 hover:bg-gray-600">
-            <Link href="/">
-              <a className="block px-4 py-1">Login</a>
-            </Link>
-          </li>
+          {user && (
+            <li className="mr-6 font-semibold text-gray-800 uppercase transition border border-gray-800 rounded-md hover:text-gray-200 hover:bg-gray-600">
+              <a className="block px-4 py-1" onClick={logout}>
+                Logout
+              </a>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
